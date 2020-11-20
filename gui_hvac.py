6d64947e2adfba8d4d26ae6bc53f09a97a3b28f7
blasher565 <72475390+blasher565@users.noreply.github.com>
2020-11-20 11:19:53 -0500

    Update gui_hvac.py
    Added changable Exterior temp to HVAC GUI

--- gui_hvac.py
+++ gui_hvac.py
@@ -5,12 +5,45 @@ Created on Tue Nov 10 15:37:22 2020
 @author: Brandon Lasher
 """
 from tkinter import *
+from math import sqrt
 
 class gui_hvac:
     def __init__(self):
         self.__hvac_obj = None       
         self.hvacCanvas = None
 
+    #
+    #Draws a trignale based on side or height referance
+    # Used X offset / Y offset as center point!
+    def drawDownArrow(self, canvas, xOffset, yOffset, v, ref="h" ):
+        
+        if ref == "s":
+            sideLen = v
+            height = sideLen * sqrt(3) / 2
+        else: #ref == "h":
+            height = v
+            sideLen = 2 * height / sqrt(3)
+        
+        p0 = (xOffset-sideLen/2, yOffset-height/2 )
+        p1 = (xOffset, yOffset+height/2)
+        p2 = (xOffset+sideLen/2, yOffset-height/2)
+        
+        return canvas.create_polygon( p0, p1, p2, fill="black")
+    
+    
+    def drawUpArrow(self, canvas, xOffset, yOffset, v, ref="h"):
+    
+        if ref == "s":
+            sideLen = v
+            height = sideLen * sqrt(3) / 2
+        else: #ref == "h":
+            height = v
+            sideLen = 2 * height / sqrt(3)    
+    
+        p0 = (xOffset-sideLen/2, yOffset + height/2)
+        p1 = ( xOffset, yOffset - height/2)
+        p2 = ( xOffset+sideLen/2, yOffset + height/2)
+        return canvas.create_polygon( p0, p1, p2, fill="black")
 
     def drawHVAC( self, parent ):
         parent.update()
@@ -35,10 +68,29 @@ class gui_hvac:
 
         self.hvacCanvas.create_text( self.xcol[1], self.yRow[2], text ="Exterior Temp:", font="Arial 14 bold", anchor="e", fill="black", tags=("extTemp"))
         self.hvacCanvas.create_text( self.xcol[2], self.yRow[2], text ="" , font="Arial 14", fill="black", anchor="w", tags=("extTempText"))
+        
+        upArrow = self.drawUpArrow( self.hvacCanvas, self.xcol[2]+75, self.yRow[2], yinc-10 )
+        downArrow = self.drawDownArrow( self.hvacCanvas, self.xcol[2]+95, self.yRow[2], yinc-10 )
+        
+        
+        #Changing the Temp here will auto update the Text via other
+        # binding
+        def incExtTemp():
+            if self.hvac:
+                self.hvac.exteriorTemp = self.hvac.exteriorTemp  + 1
+            
+        def decExtTemp():
+            if self.hvac:
+                self.hvac.exteriorTemp = self.hvac.exteriorTemp  - 1
+        
+        self.hvacCanvas.tag_bind( upArrow, "<Button-1>", lambda e:  incExtTemp() )
+        self.hvacCanvas.tag_bind( downArrow, "<Button-1>", lambda e:  decExtTemp() )
+        
+        
 
-        self.hvacCanvas.create_text( self.xcol[1], self.yRow[3], text ="CFM:", font="Arial 14 bold", fill="black", anchor="e", tags=("cfm"))
-        self.hvacCanvas.create_text( self.xcol[2], self.yRow[3], text ="" , font="Arial 14", fill="black", anchor="w", tags=("cfmText"))
-                
+        #self.hvacCanvas.create_text( self.xcol[1], self.yRow[3], text ="CFM:", font="Arial 14 bold", fill="black", anchor="e", tags=("cfm"))
+        #self.hvacCanvas.create_text( self.xcol[2], self.yRow[3], text ="" , font="Arial 14", fill="black", anchor="w", tags=("cfmText"))
+        
         #Set the initial Text
         self.updateAll()
         
@@ -78,7 +130,7 @@ class gui_hvac:
         self.__hvac_obj.bind_to_tag("fanOn", self.updateFanMode)
         self.__hvac_obj.bind_to_tag("fanAlwaysOn", self.updateFanMode)
         self.__hvac_obj.bind_to_tag("exteriorTemp",  self.updateExtTemp)
-        self.__hvac_obj.bind_to_tag("cfm", self.updateCFM)
+        #self.__hvac_obj.bind_to_tag("cfm", self.updateCFM)
     
     
 if __name__ == "__main__":
